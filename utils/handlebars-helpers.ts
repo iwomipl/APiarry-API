b7c9ee0fd4b6d92e5f7b08cd7b0913749c2bf43e
Iwo Matecki <iwo@iwomi.pl>
2022-06-10 16:07:41 +0200

    Add sorting and form to home.router and handlebars

--- utils/handlebars-helpers.ts
+++ utils/handlebars-helpers.ts
@@ -6,4 +6,5 @@ export const handlebarsHelpers = {
     checkIfListExists: (array: ApiaryRecord[])=> array.length > 0,
     findMin: (array: ApiaryRecord[])=> array[0].startTime,
     findMax: (array: ApiaryRecord[])=> array[array.length-1].startTime,
+    minOrMax: (array: ApiaryRecord[])=> array[array.length-1].startTime > array[0].startTime,
 }
